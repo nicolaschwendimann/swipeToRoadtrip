e30d13d0efb86251ddb88c0ba2210a19c270ec3b
saskia <48206716+saskiaschweizer@users.noreply.github.com>
2020-10-19 17:00:18 +0200

    CHANGES: add intro menu, add swipe basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:swipeToRoadtrip/app/roadtrip_app.dart';
+import 'package:swipeToRoadtrip/ui/home/description_home.dart';
+import 'package:swipeToRoadtrip/ui/home/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(RoadtripApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class RoadtripApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run the app on.
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  // This widget is the home page of your application. It is stateful, meaning
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done by the _incrementCounter method above.
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/models/people_list.dart
@@ -0,0 +1,13 @@
+import 'package:swipeToRoadtrip/ui/home/matching_card.dart';
+
+List<MatchCard> peoples = [
+  MatchCard(
+      "screenshot 1", 'assets/images/screenShot_1.jpg', 'GR', 'Landschaft'),
+  MatchCard(
+      "screenshot 2", 'assets/images/screenShot_2.jpg', 'ZH', 'Landschaft'),
+  MatchCard("screenshot 3", 'assets/images/screenShot_3.jpg', 'GL', 'Natur'),
+  MatchCard("screenshot 4", 'assets/images/screenShot_4.jpg', 'SZ', 'Berge'),
+  MatchCard("screenshot 5", 'assets/images/screenShot_5.jpg', 'VS', 'Action'),
+  MatchCard(
+      "screenshot 6", 'assets/images/screenShot_6.jpg', 'AG', 'Landschaft'),
+];
